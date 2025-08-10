4cda9c939b2fabdb24afd78c6dd616f797666bb7
unknown <haidaralghifari112@gmail.com>
2025-08-11 00:54:37 +0700

    add import yang ketinggalan

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PyQt5.QtWidgets import QDialog
 from PyQt5.QtCore import Qt, QSize, QTimer
 from PyQt5.QtGui import QCursor, QColor, QIcon
 
+import resource
+
 class loginpage(QDialog):
     def __init__(self):
         super().__init__()
